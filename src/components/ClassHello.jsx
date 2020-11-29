2845a78f72b4c12457c4756e4733010c2893b6fb
wangxiao <yanrancanfei@gmail.com>
2020-11-29 23:44:21 +0800

    init

--- src/components/ClassHello.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default class ClassHello extends React.Component {
-    render (){
-        console.log(this.props)
-        return <div> 这是一个div</div>
-    }
-}
--- src/components/Hello.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-
-export default function Hello(props) {
-    console.log(props)
-    return <div>这是一个 hello 组件{props.name}</div>
-}
